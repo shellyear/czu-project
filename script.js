dfd27148a8153394ba614c20a68cb176690f1e3c
Nurbek Yelesh <boolevafunkce@gmail.com>
2020-10-22 03:41:42 +0200

    finished

--- script.js
+++ script.js
@@ -5,9 +5,29 @@ const allButtons = document.getElementsByClassName('button');
 
 // When the user clicks on the button, open the modal
 for (i = 0; i < allButtons.length; i++) {
+  
+  const BookCard = allButtons[i].closest('article');
+
+  const BookContent = BookCard.children[1];
+  const BookTitle = BookContent.children[0].innerHTML;
+  const BookPrice = BookContent.children[2].children[0].innerHTML;
+  // console.log('Article', BookContent);
+  console.log('Article title', BookPrice)
+
   allButtons[i].onclick = function() {
-    modal.style.display = "block";  
-    console.log('modal', modal.children);
+    modal.style.display = "block";
+    const ModalContent = modal.children[0];
+    const CreditCard = ModalContent.children[1];
+    const Form = CreditCard.children[1];
+    let FormTitle = Form.children[0];
+
+    // set FormTitle to BookTitle
+    FormTitle.innerHTML = BookTitle; 
+
+
+    //set FormPrice to bookPrice
+    let Price = Form.children[1].children[1];
+    Price.value = BookPrice;
   }  
 }
 
