e41dacab6643a9cf5baef6b614074dd2a1271499
Nurbek Yelesh <boolevafunkce@gmail.com>
2020-12-13 19:30:14 +0100

    another commit

---
+++ cart.js
@@ -0,0 +1,110 @@
+if (document.readyState == 'loading')
+{
+  document.addEventListener('DOMContentLoaded', ready)
+}
+else{
+  ready()
+};
+
+function ready(){
+  var removeCartItemButtons = document.getElementsByClassName('btn-delete')
+  // console.log(removeCartItemButtons)
+  for (var i = 0; i < removeCartItemButtons.length; i++) {
+      var button = removeCartItemButtons[i]
+      button.addEventListener('click',removeCartItem)
+  }
+  var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+  for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]
+    input.addEventListener('change',quantityChanged)
+  }
+
+
+
+  function updateCartTotal() {
+    let cartItemContainer = document.getElementsByClassName('cart-items')[0]
+    let cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    // let localCardBasket = JSON.parse(localStorage.getItem('cardBasket'));
+    let total = 0;
+    for (var i = 0; i < cartRows.length; i++) {
+      let cartRow = cartRows[i]
+      let priceElement = cartRow.getElementsByClassName('cart-price')[0]
+      let quantityElement = cartRow.getElementsByClassName('cart-quantity-input') [0]
+      let price = parseFloat(priceElement.innerText.replace(' Kč',''))
+      let quantity = quantityElement.value
+      total = total + (price * quantity)
+    }
+    total = Math.round(total * 100) / 100
+    document.getElementsByClassName('cart-total-price')[0].innerText = total + ' Kč'
+  };
+
+  function quantityChanged(event) {
+    var input = event.target;
+    const itemId = Number(input.id);
+    
+    if (isNaN(input.value)) input.value = 1;
+    else if (input.value <= 0)  {
+      console.log('yes');
+      let localStorageItems = JSON.parse(localStorage.getItem('cardBasket'));
+      console.log('localStorageItems', localStorageItems)
+      let newlocalStorageItems = localStorageItems.filter(el => {
+        return el.id !== itemId;
+      });
+
+      localStorage.setItem('cardBasket', JSON.stringify(newlocalStorageItems));
+      document.getElementById(itemId).remove();
+    }
+    updateCartTotal()
+  };
+};
+
+function removeCartItem(event) {
+  let itemId = event.target.id;
+  const localStorageItems = JSON.parse(localStorage.getItem('cardBasket'));
+  const newlocalStorageItems = localStorageItems.filter((item) => {
+    if (item.id === itemId) {
+      if (item.count === 1) {
+        localStorageItems.filter(el => el.count-- !== 0);
+      }
+      return { ...item, count: item.count--}
+    };
+  });
+  localStorage.setItem('cardBasket', JSON.stringify(newlocalStorageItems));
+
+  let buttonClicked = event.target;
+  buttonClicked.parentElement.parentElement.remove();
+
+  updateCartTotal()
+  updateCartTotal()
+};
+
+
+JSON.parse(localStorage.getItem('cardBasket')).forEach(({ title, price, count, id }) => { 
+  let imgUrl; 
+  if (title === 'Jaws') imgUrl = './static/bookImg2.jpg';
+  if (title === 'Kačer Donald Vs. Batman : The Dark menace') imgUrl = './static/bookImg4.jpg';
+  if (title === 'Harry Potter: Prisoner of Azkaban') imgUrl = './static/bookImg3.jpg';
+  if (title === 'Marťan') imgUrl = './static/bookImg.jpg'; 
+  const target = document.getElementsByClassName('cart-items')[0];
+  const mainDiv = document.createElement('div');
+  mainDiv.setAttribute('class', 'cart-row');
+  mainDiv.setAttribute('id', id);
+  // console.log('target', target);
+  // console.log('mainDiv', mainDiv.id);
+  // const id = title.split(" ");
+  console.log('id', id);
+  mainDiv.innerHTML = `
+    <div class="cart-item cart-column">
+        <img class="cart-item-image" src=${imgUrl} alt="book" width="100" height="100">
+        <span class="cart-item-title">${title}</span>
+    </div>
+    <span class="cart-price cart-column">${price}</span>
+    <div class="cart-quantity cart-column">
+        <input class="cart-quantity-input" type="number" value=${count} id=${id}>
+        <button class="btn-delete" type="button" id=${id}>Odstranit z košíku</button>
+    </div>
+  `;
+  target.appendChild(mainDiv);
+});
+
+
--- script.js
+++ script.js
@@ -1,48 +1,46 @@
-let modal = document.getElementById("myModal");
 
-// Get all buttons that opens the modal
-const allButtons = document.getElementsByClassName('button');
 
-// When the user clicks on the button, open the modal
-for (i = 0; i < allButtons.length; i++) {
+let cardBasket = [];
+if (!localStorage.getItem('cardBasket')) {
+  localStorage.setItem('cardBasket', JSON.stringify(cardBasket));
+}
+
+function onClickMenu(){
+  document.getElementById("menu").classList.toggle("change");
+  document.getElementById("mobile-nav").classList.toggle("change");
+};
+
+/* {
+  title: string,
+  price: number,
+  autor: string,
+} */
+function addToBasket(cardData) { 
+  const { title, price } = cardData;
+  console.log(title, price);
+  const pastData = JSON.parse(localStorage.getItem('cardBasket'));
+  // if (pastData) {} cardBasket = [...pastData];
+  console.log('pastData', localStorage.getItem('cardBasket'));
   
-  const BookCard = allButtons[i].closest('article');
+  if (title, price) {
+    console.log('cardBasket in condition', cardBasket);
+    let exists  = cardBasket.filter(el => el.title === title).length;
+    console.log('exists', exists);
+    if (exists) {
+      cardBasket = cardBasket.map(item => {
+        if (item.title === title) {
+          return { ...item, count: item.count+1};
+        } else {
+          return item;
+        }
+      });
+    } else {
+      console.log('id random', Math.random);
+      console.log('first time');
+      cardBasket = [...cardBasket, { ...cardData, count: 1, id: Math.random() }];
+    };
+  };
+  const newData = JSON.stringify(cardBasket);
 
-  const BookContent = BookCard.children[1];
-  const BookTitle = BookContent.children[0].innerHTML;
-  const BookPrice = BookContent.children[2].children[0].innerHTML;
-  // console.log('Article', BookContent);
-  console.log('Article title', BookPrice)
-
-  allButtons[i].onclick = function() {
-    modal.style.display = "block";
-    const ModalContent = modal.children[0];
-    const CreditCard = ModalContent.children[1];
-    const Form = CreditCard.children[1];
-    let FormTitle = Form.children[0];
-
-    // set FormTitle to BookTitle
-    FormTitle.innerHTML = BookTitle; 
-
-
-    //set FormPrice to bookPrice
-    let Price = Form.children[1].children[1];
-    Price.value = BookPrice;
-  }  
-}
-
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+  localStorage.setItem('cardBasket', newData);
+};
